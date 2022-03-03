6bbec3be0c9e5ba31aa433d0296b861fdfbedd64
Federico Soria <fedesoria.dev@gmail.com>
2022-03-03 19:42:18 -0300

    feat(posts): add relationship between posts and users (author)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -28,4 +29,12 @@ class Post extends Model
     protected $casts = [
         'published_at' => 'datetime'
     ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
 }
