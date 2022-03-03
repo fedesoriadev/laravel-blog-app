425eeb7004a2fb4b7d34f12ddf034a3cdacd50fb
Federico Soria <fedesoria.dev@gmail.com>
2022-03-03 20:05:26 -0300

    feat(roles): add relationship between roles and users

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Role extends Model
 {
@@ -16,4 +17,12 @@ class Role extends Model
         'name',
         'description'
     ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
+    }
 }
