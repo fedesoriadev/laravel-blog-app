d4f2ea74fb2af95db2a298fba45c12b8bdcba71a
Federico Soria <fedesoria.dev@gmail.com>
2022-04-21 15:21:02 -0300

    style(admin): fallback to Fira Sans in admin area

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Inter', ...defaultTheme.fontFamily.sans],
+                sans: ['Inter', 'Fira Sans', ...defaultTheme.fontFamily.sans],
             },
         },
     },
