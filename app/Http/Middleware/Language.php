310cdfa6a4c342f3bae4731c4ee15377df0f4263
Federico Soria <fedesoria.dev@gmail.com>
2022-04-24 21:10:01 -0300

    fix(core): fix return type declaration in Language Middleware

--- app/Http/Middleware/Language.php
+++ app/Http/Middleware/Language.php
@@ -3,7 +3,10 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class Language
 {
@@ -12,9 +15,9 @@ class Language
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse|\Illuminate\Http\JsonResponse
      */
-    public function handle(Request $request, Closure $next): \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+    public function handle(Request $request, Closure $next): Response|RedirectResponse|JsonResponse
     {
         if (session()->has('locale') && \App\Enums\Language::tryFrom(session('locale'))) {
             app()->setLocale(session('locale'));
